06eaaa1568af83cf33b425acbf450e8daeadb637
Kenta Kase <kesin1202000@gmail.com>
2014-01-03 01:18:43 +0900

    freqfile形式から索引付けを行えるように修正

--- src/indexing.java
+++ src/indexing.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -13,17 +18,85 @@ public class indexing {
         writer.addDocument(doc);
 	}
 
+    /**
+     *
+     * @param path freqfileのパス
+     * @return スペース区切りの単語が連続する文字列
+     * @throws IOException
+     */
+	public static HashMap<String, HashMap<String, Integer>> parseFreqfile(String path) throws IOException{
+		HashMap<String, HashMap<String, Integer>> idWordTfHash = new HashMap<String, HashMap<String, Integer>>();
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String line;
+		String id = "";
+		HashMap<String, Integer> wordTf = new HashMap<String, Integer>();
+		while((line = br.readLine()) != null){
+			if (line.startsWith("@")){
+				if (!id.isEmpty()){
+				    idWordTfHash.put(id, wordTf);
+				}
+
+                wordTf = new HashMap<String, Integer>();
+				id = line.substring(1);
+			}
+			else{
+				String[] tfWord = line.split(" ");
+				wordTf.put(tfWord[1], new Integer(tfWord[0]));
+			}
+		}
+		br.close();
+		return idWordTfHash;
+	}
+
+	//repeatNumの数だけwordが入っている配列を作成する
+	public static String[] repatStringWithNumber(String word, Integer repeatNum) {
+		ArrayList<String> strings = new ArrayList<String>();
+		for (int i=0; i<repeatNum; i++){
+			strings.add(word);
+		}
+		return strings.toArray(new String[0]);
+	}
+
+	//splitter区切りでwordsを連結する。perlやpythonのjoinと同じ
+	public static String joinWithSplitter(String[] words, String splitter){
+		String string = "";
+		for (String word: words){
+			if (!string.isEmpty()){
+				string += splitter;
+			}
+			string += word;
+		}
+		return string;
+	}
+
 	public static void main(String[] args) throws IOException {
 		SpokendocBaseline spokendoc = new SpokendocBaseline("index");
 
 		// Indexing
 		IndexWriter writer = spokendoc.getIndexWriter();
-        addDoc(writer, "doc1", "This is the text to be indexed");
-        addDoc(writer, "doc2", "High score text text");
-        addDoc(writer, "doc3", "Lower score. Score is maybe normalized using text length");
-        addDoc(writer, "doc4", "text");
+		HashMap<String, HashMap<String, Integer>> idWordTfHash = parseFreqfile("freqfile");
+		for (Map.Entry<String, HashMap<String, Integer>> idWordTf : idWordTfHash.entrySet()){
+			String docId = idWordTf.getKey();
+			String indexString = "";
+			HashMap<String, Integer> wordTfHash = idWordTf.getValue();
+			for (Map.Entry<String, Integer> wordTf : wordTfHash.entrySet()){
+				String word = wordTf.getKey();
+				Integer num = wordTf.getValue();
+				//numの回数だけwordを繰り返す
+				//例: "word word word"
+				String[] repeatStrings = repatStringWithNumber(word, num);
+				String repeatString = joinWithSplitter(repeatStrings, " ");
+				//docIdに含まれていた単語をスペース区切りで1つの文字列にする
+				//例: "word1 word1 word2 word3"
+				if (!indexString.isEmpty()){
+					indexString += " ";
+				}
+				indexString += repeatString;
+			}
+			addDoc(writer, docId, indexString);
+		}
 	    writer.close();
-	    
+
 	    System.out.println("Done indexing!");
 	}
 }
